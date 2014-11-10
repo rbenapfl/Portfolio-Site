7b9b2fe965a746e14b206901fe16330ec24e36d7
Robert Benapfl <rabsax@comcast.net>
2014-11-10 11:33:43 -0800

    clean up buggy variable to be outside of controller

--- javascripts/mySite.js
+++ javascripts/mySite.js
@@ -14,22 +14,9 @@
 	});
 
 	app.controller('QuicksortController', function(Shuffler) {
-		this.startingArray = [
-			{letter:'W',value:1,status:'unsolved'},
-			{letter:'e',value:2,status:'unsolved'},
-			{letter:'b',value:3,status:'unsolved'},
-			{letter:'D',value:4,status:'unsolved'},
-			{letter:'e',value:5,status:'unsolved'},
-			{letter:'v',value:6,status:'unsolved'},
-			{letter:'e',value:7,status:'unsolved'},
-			{letter:'l',value:8,status:'unsolved'},
-			{letter:'o',value:9,status:'unsolved'},
-			{letter:'p',value:10,status:'unsolved'},
-			{letter:'e',value:11,status:'unsolved'},
-			{letter:'r',value:12,status:'unsolved'}
-		]
-		this.shuffledArray = Shuffler.shuffleArray(this.startingArray);
-		this.messages = [this.shuffledArray,[{letter:'yesthisworksyay'}]]
+		this.startingMessage = homeMessage
+		this.shuffledMessage = Shuffler.shuffleArray(this.startingMessage);
+		this.messages = [this.shuffledMessage,[{letter:'yesthisworksyay'}]]
 	});
 
 	app.service('Shuffler', function() {
@@ -44,5 +31,20 @@
 			return shuffled
     	};
 	});
+
+	var homeMessage = [
+			{letter:'W',value:1,status:'unsolved'},
+			{letter:'e',value:2,status:'unsolved'},
+			{letter:'b',value:3,status:'unsolved'},
+			{letter:'D',value:4,status:'unsolved'},
+			{letter:'e',value:5,status:'unsolved'},
+			{letter:'v',value:6,status:'unsolved'},
+			{letter:'e',value:7,status:'unsolved'},
+			{letter:'l',value:8,status:'unsolved'},
+			{letter:'o',value:9,status:'unsolved'},
+			{letter:'p',value:10,status:'unsolved'},
+			{letter:'e',value:11,status:'unsolved'},
+			{letter:'r',value:12,status:'unsolved'}
+	]
 
 })();
