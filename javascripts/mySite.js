e96029b7791b887df5d8ccae10511297af3a6b10
Robert Benapfl <rabsax@comcast.net>
2014-11-10 11:00:10 -0800

    calm before the storm (add nested lists for quicksort display on dom)

--- javascripts/mySite.js
+++ javascripts/mySite.js
@@ -1,7 +1,6 @@
 (function(){
 	var app = angular.module('mySite', [ ])
 
-
 	app.controller('PanelController', function() {
 		this.activePanel = 1;
 
@@ -15,8 +14,22 @@
 	});
 
 	app.controller('QuicksortController', function(Shuffler) {
-		this.startingArray = Shuffler.shuffleArray(['W','e','b','D','e','v','e','l','o','p','e','r']);
-		this.startingMessage = this.startingArray.join('')
+		this.startingArray = [
+			{letter:'W',value:1,status:'unsolved'},
+			{letter:'e',value:2,status:'unsolved'},
+			{letter:'b',value:3,status:'unsolved'},
+			{letter:'D',value:4,status:'unsolved'},
+			{letter:'e',value:5,status:'unsolved'},
+			{letter:'v',value:6,status:'unsolved'},
+			{letter:'e',value:7,status:'unsolved'},
+			{letter:'l',value:8,status:'unsolved'},
+			{letter:'o',value:9,status:'unsolved'},
+			{letter:'p',value:10,status:'unsolved'},
+			{letter:'e',value:11,status:'unsolved'},
+			{letter:'r',value:12,status:'unsolved'}
+		]
+		this.shuffledArray = Shuffler.shuffleArray(this.startingArray);
+		this.messages = [this.shuffledArray,[{letter:'yesthisworksyay'}]]
 	});
 
 	app.service('Shuffler', function() {
